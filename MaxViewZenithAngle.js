1e66bc42ac0bb7da9e5116815fcf46ad7c020467
agdc2005 <59077057+agdc2005@users.noreply.github.com>
2022-03-08 17:59:31 -0500

    Add files via upload

--- MaxViewZenithAngle.js
+++ MaxViewZenithAngle.js
@@ -7,7 +7,7 @@ import React from "react";
 //Keys help React identify which items have changed, are added, or are removed. 
 //Keys should be given to the elements inside the array to give the elements a stable identity:
 
-export const MaxViewZenithAngle = ({ maxvza,vzaval,onVZAChange }) => {
+export const MaxViewZenithAngle = ({ maxvza, vzaval, onVZAChange }) => {
 
   const handleVZAChange = (e) => {
     console.log(e.target.value);
